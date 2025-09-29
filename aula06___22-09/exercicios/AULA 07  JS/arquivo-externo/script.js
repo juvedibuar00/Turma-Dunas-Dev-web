34036cfba0ab7f5c84dab0f0bdebc03ac256198a
juvedibuar00 <batchidibuar@gmail.com>
2025-09-29 08:34:38 -0300

    js

---
+++ aula06___22-09/exercicios/AULA 07  JS/arquivo-externo/script.js
@@ -0,0 +1,3 @@
+console.log("Executando JavaScript de um arquivo externo!");
+alert("Olá, mundo de um arquivo externo!");
+document.write("Este texto foi escrito usando JavaScript em um arquivo externo.");
